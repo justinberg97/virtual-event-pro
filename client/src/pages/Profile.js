d40c33dc928509f3ca26ff2fb68d24515c0de37b
gsalma99 <salmagonzalezzz10011@gmail.com>
2023-01-30 21:27:50 -0600

    updated profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -9,19 +9,19 @@ import {
 
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations';
-import { removeBookId } from '../utils/localStorage';
+import { REMOVE_EVENT } from '../utils/mutations';
+import { removeEventId } from '../utils/localStorage';
 
 import Auth from '../utils/auth';
 
-const SavedBooks = () => {
+const SavedEvents = () => {
   const { loading, data } = useQuery(QUERY_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeEvent, { error }] = useMutation(REMOVE_EVENT);
 
   const userData = data?.me || {};
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
+  const handleDeleteEvent = async (eventId) => {
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -30,12 +30,12 @@ const SavedBooks = () => {
     }
 
     try {
-      const { data } = await removeBook({
-        variables: { bookId },
+      const { data } = await removeEvent({
+        variables: { eventId },
       });
 
       // upon success, remove book's id from localStorage
-      removeBookId(bookId);
+      removeEventId(eventId);
     } catch (err) {
       console.error(err);
     }
@@ -54,32 +54,32 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks?.length
-            ? `Viewing ${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? 'book' : 'books'
+          {userData.SavedEvents?.length
+            ? `Viewing ${userData.savedEvents.length} saved ${
+                userData.savedEvents.length === 1 ? 'event' : 'events'
               }:`
-            : 'You have no saved books!'}
+            : 'You have no saved events!!!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks?.map((book) => {
+          {userData.savedEvents?.map((event) => {
             return (
-              <Card key={book.bookId} border="dark">
-                {book.image ? (
+              <Card key={event.eventId} border="dark">
+                {event.image ? (
                   <Card.Img
-                    src={book.image}
-                    alt={`The cover for ${book.title}`}
+                    src={event.image}
+                    alt={`The cover for ${event.title}`}
                     variant="top"
                   />
                 ) : null}
                 <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <p className="small">Authors: {book.authors}</p>
-                  <Card.Text>{book.description}</Card.Text>
+                  <Card.Title>{event.title}</Card.Title>
+                  <p className="small">Authors: {event.host}</p>
+                  <Card.Text>{event.description}</Card.Text>
                   <Button
                     className="btn-block btn-danger"
-                    onClick={() => handleDeleteBook(book.bookId)}
+                    onClick={() => handleDeleteEvents(event.eventId)}
                   >
-                    Delete this Book!
+                    Delete this Event!
                   </Button>
                 </Card.Body>
               </Card>
@@ -91,4 +91,4 @@ const SavedBooks = () => {
   );
 };
 
-export default SavedBooks;
+export default SavedEvents;
