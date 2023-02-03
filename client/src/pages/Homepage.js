919faef66a67bc5cfae0d2e5a4f7b3bc351cb95f
gsalma99 <salmagonzalezzz10011@gmail.com>
2023-02-02 22:14:26 -0600

    updated some css

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -4,16 +4,17 @@ import { Container, Jumbotron, Card, CardColumns } from "react-bootstrap";
 function HomePage() {
   return (
     <Container>
-      <Jumbotron style={{ backgroundColor: "#7c4dff", color: "white" }}>
-        <h1 className="text-center">Welcome to Virtual Event Pro</h1>
+      <Jumbotron style={{ backgroundColor: "#7c4dff", color: "white", borderRadius: "50%", width: ""}}>
+        <h1 className="text-center, ">Welcome to Virtual Event Pro</h1>
       </Jumbotron>
       <CardColumns>
         <Card style={{ backgroundColor: "white", color: "#7c4dff" }}>
           <Card.Body>
             <Card.Title>Virtual Event Pro</Card.Title>
-            <Card.Text>
+            <Card.Text style={{ borderRadius: "50%, width: 100px: height: 100px," }}>
               Organize and host events with ease using Virtual Event Pro.  Signup to begin, or Login to continue to your profile!
-            </Card.Text>
+              </Card.Text>
+
           </Card.Body>
         </Card>
       </CardColumns>
